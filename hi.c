dca5bc4888b9827e77310f7e1a4ed24f1c37569a
archanapoojary <achhupujari@gmail.com>
2025-10-03 15:41:28 +0530

    changing

--- hi.c
+++ hi.c
@@ -3,8 +3,15 @@
 int main() {
     printf("Hello, world!\n");
     printf("Hello, world!\n");
-     printf("Hello, world!\n");
-     printf("Hello, world!\n");
-     printf("Hello, world!\n");
+    printf("Hello, world!\n");
+    printf("Hello, world!\n");
+    printf("Hello, world!\n");
+    int age = 20;
+
+    if(age >= 18) {
+            printf("You are an adult.\n");}
+    else {
+            printf("You are a minor.\n");
+     }
     return 0;
 }
